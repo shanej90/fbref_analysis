1f6b75204a15230b5e124ebec783676107e38d63
Shane Jackson <shane.jackson@gmx.co.uk>
2025-05-10 11:07:50 +0100

    get parameters

--- utils/parameters.py
+++ utils/parameters.py
@@ -3,12 +3,14 @@
 #########################################
 
 ### IMPORTS
-from
+from ScraperFC import FBref
 
 ### PARAMATERS
 
 #leagues
-leagues = []
+leagues = ["Big 5 combined"]
 
 #seasons
-seasons = []
+fbr = FBref()
+all_seasons = fbr.get_valid_seasons("Big 5 combined")
+seasons = list(all_seasons.keys())[0:5] # current season plus previous five
